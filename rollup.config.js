9947911a61a27cd9ceb3c59f70e62fdb3a45c70f
Jack Steam <jacksteamdev@gmail.com>
2020-08-31 15:39:52 -0500

    Add initial config files

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,14 @@
-// rollup.config.js
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 
-import { chromeExtension } from 'rollup-plugin-chrome-extension'
+import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension'
 
 export default {
-  input: 'src/manifest.json',
-  // input: 'src/background.js',
+  input: ['src/manifest.json'],
   output: {
     dir: 'dist',
     format: 'esm',
   },
   // always put chromeExtension() before other plugins
-  plugins: [
-    chromeExtension(),
-    resolve(),
-    commonjs()
-  ],
+  plugins: [chromeExtension(), simpleReloader(), resolve(), commonjs()],
 }
