e438ef591490f71b415810e22df3b1941f296ff7
Jack Steam <jacksteamdev@gmail.com>
2020-10-19 16:19:00 -0500

    [chore] update configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,13 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+/* eslint-env node */
+
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import zip from 'rollup-plugin-zip'
+import { emptyDir } from 'rollup-plugin-empty-dir'
 
 import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension'
+
+const p = process.env.NODE_ENV === 'production'
 
 export default {
   input: ['src/manifest.json'],
@@ -9,6 +15,18 @@ export default {
     dir: 'dist',
     format: 'esm',
   },
-  // always put chromeExtension() before other plugins
-  plugins: [chromeExtension(), simpleReloader(), resolve(), commonjs()],
+  plugins: [
+    // always put chromeExtension() before other plugins
+    chromeExtension(),
+    // includes an automatic reloader in watch mode
+    simpleReloader(),
+    // resolves node modules
+    resolve(),
+    // converts libraries that use commonjs
+    commonjs(),
+    // empties the dist for each build
+    emptyDir(),
+    // creates a zip to upload to the Chrome Web Store :)
+    p && zip({ dir: 'releases' }),
+  ],
 }
